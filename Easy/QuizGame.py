ebc21953155b0d2f1c0ba9c20e382f44003dfd9f
Jasmit Malhi <151409852+JasmitMalhi@users.noreply.github.com>
2024-04-02 09:19:44 -0400

    Add files via upload

---
+++ Easy/QuizGame.py
@@ -0,0 +1,44 @@
+print("Welcome to my game, press 1 to proceed, press 2 to exit")
+userInput=input()
+if '1' in userInput:
+    print("Whats 1+1")
+    A=input()
+     #A
+    if "2" in A:
+        print("Correct")
+    else:
+        print("Incorrect")
+    print("Whats 2+1")
+    B=input()
+     #B
+    if "3" in B:
+        print("Correct")
+    else:
+        print("Incorrect")
+    print("Whats 3+1")
+    C=input()
+     #C
+    if "4" in C:
+        print("Correct")
+    else:
+        print("Incorrect")
+    print("Whats 4+1")
+    D=input()
+     #D
+    if "5" in D:
+        print("Correct")
+    else:
+        print("Incorrect")
+    print("Whats 5+1")
+    E=input()
+   
+         #E
+    if "6" in E:
+        print("Correct")
+    else:
+        print("Incorrect")
+else:
+    print("You Quit!")
+
+
+print("Done")
